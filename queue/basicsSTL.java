0310af92e043c4caf11a4265a22f829635dcdc18
rijans <makwanarijans@gmail.com>
2025-09-04 13:45:45 +0530

    Files added

--- queue/basicsSTL.java
+++ queue/basicsSTL.java
@@ -1,5 +1,16 @@
 package queue;
 
-public class basicsSTL {
-  
+import java.util.*;
+
+public class BasicsSTL {
+    public static void main(String[] args) {
+        java.util.LinkedList<Integer> q = new java.util.LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(1);
+        System.out.println(q);
+    }
 }
